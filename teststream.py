23f374458484dcf5db812bd866a9727dc04c3d84
Shu-Kei <125178921+Shu-Kei@users.noreply.github.com>
2023-07-06 17:25:39 +0700

    Update teststream.py

--- teststream.py
+++ teststream.py
@@ -29,7 +29,7 @@ def autoplay_audio(file_path: str):
             unsafe_allow_html=True,
         )    
  
-llm = ChatOpenAI(openai_api_key="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",temperature=0.0)
+llm = ChatOpenAI(openai_api_key="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",temperature=0.0)
 r = sr.Recognizer()
 
 if "memory" not in st.session_state:
